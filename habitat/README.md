4de1cfdbfb6e5dad4d18260d25ae672a357c6854
Christian Nunciato <chris@nunciato.org>
2018-02-13 16:22:51 -0800

    gitignore, hab stuff

---
+++ habitat/README.md
@@ -0,0 +1,9 @@
+# Habitat package: testapp
+
+## Description
+
+Provide a brief description of the `testapp` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `testapp` plan
---
+++ habitat/plan.sh
@@ -0,0 +1,13 @@
+pkg_name=testapp2
+pkg_origin=cnunciato
+pkg_version="0.1.0"
+pkg_maintainer="Christian Nunciato <chris@nunciato.org>"
+pkg_license=("MIT")
+
+do_install() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
